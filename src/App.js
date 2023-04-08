c6751727d4accd19003c231d22da3f18dac2aebc
RishabhGupta-04 <rg074523@gmail.com>
2023-04-09 01:09:08 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Create from "./Components/create/create";
+import Read from "./Components/read/read";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Update from "./Components/update/update";
+import Delete from "./Components/delete/delete";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main-2">
+        <h3 className="main-2-h">Employee Management Operations</h3>
+      </div>
+      <Router>
+        <Routes>
+          <Route exact={true} path="/" element={<Create />} />
+          <Route exact={true} path="/read" element={<Read />} />
+          <Route exact={true} path="/update" element={<Update />} />
+          <Route exact={true} path="/delete" element={<Delete />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
